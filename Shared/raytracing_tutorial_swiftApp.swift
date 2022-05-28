60b67cacc9dc0787e266e08234307f7dff4fffcc
thu_yumeng <yumengch727727@gmail.com>
2022-05-28 08:56:24 +0800

    Initial Commit

---
+++ Shared/raytracing_tutorial_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  raytracing_tutorial_swiftApp.swift
+//  Shared
+//
+//  Created by 于蒙 on 2022/5/28.
+//
+
+import SwiftUI
+
+@main
+struct raytracing_tutorial_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
